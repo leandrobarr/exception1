79a59bc48e24ffab3fd26664583543b9c87f8a8c
leandrobarr <leandrobarr@yahoo.com.br>
2022-01-24 14:18:55 -0300

    very bad solution1

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,54 +1,53 @@
 package application;
 
-import entities.Reservation;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
+import entities.Reservation;
+
 public class Program {
+
     public static void main(String[] args) throws ParseException {
 
         Scanner sc = new Scanner(System.in);
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-        System.out.print("Quarto número: ");
-        int numero = sc.nextInt();
-        System.out.print("Check-in date (dd/mm/yyyy): ");
-        Date checkin = sdf.parse(sc.next());
-        System.out.print("Check-out date (dd/mm/yyyy): ");
-        Date checkout = sdf.parse(sc.next());
+        System.out.print("Room number: ");
+        int number = sc.nextInt();
+        System.out.print("Check-in date (dd/MM/yyyy): ");
+        Date checkIn = sdf.parse(sc.next());
+        System.out.print("Check-out date (dd/MM/yyyy): ");
+        Date checkOut = sdf.parse(sc.next());
 
-        Reservation reservation = null;
-            if (!checkout.after(checkin)) {
-                System.out.println("Erro: a data de checkout deve ser posterior a data de checkin");
-            } else {
-                reservation = new Reservation(numero, checkin, checkout);
-                System.out.println("Reservation: " + reservation);
+        if (!checkOut.after(checkIn)) {
+            System.out.println("Error in reservation: Check-out date must be after check-in date");
+        }
+        else {
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
+            System.out.println("Reservation: " + reservation);
 
-                System.out.println();
-                System.out.print("Insira a data para atualizar a reserva: ");
-
-            System.out.print("Check-in date (dd/mm/yyyy): ");
-            checkin = sdf.parse(sc.next());
-            System.out.print("Check-out date (dd/mm/yyyy): ");
-            checkout = sdf.parse(sc.next());
+            System.out.println();
+            System.out.println("Enter data to update the reservation:");
+            System.out.print("Check-in date (dd/MM/yyyy): ");
+            checkIn = sdf.parse(sc.next());
+            System.out.print("Check-out date (dd/MM/yyyy): ");
+            checkOut = sdf.parse(sc.next());
 
             Date now = new Date();
-            if (checkin.before(now) || checkout.before(now)) {
-                System.out.println("Erro na reserva: as datas de reserva para atualização devem ser datas futuras ");
+            if (checkIn.before(now) || checkOut.before(now)) {
+                System.out.println("Error in reservation: Reservation dates for update must be future dates");
             }
-            else if (!checkout.after(checkin)){
-                System.out.println("Erro na reserva: a data de check-out deve ser posterior a data de check-in");
+            else if (!checkOut.after(checkIn)) {
+                System.out.println("Error in reservation: Check-out date must be after check-in date");
             }
             else {
-                reservation.updateDates(checkin, checkout);
+                reservation.updateDates(checkIn, checkOut);
                 System.out.println("Reservation: " + reservation);
             }
-      }
+        }
 
         sc.close();
-
     }
 }
--- src/entities/Reservation.java
+++ src/entities/Reservation.java
@@ -6,54 +6,54 @@ import java.util.concurrent.TimeUnit;
 
 public class Reservation {
 
-    private Integer numeroQuarto;
-    private Date checkin;
-    private Date checkout;
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut;
 
-    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-    public Reservation(Integer numeroQuarto, Date checkin, Date checkout) {
-        this.numeroQuarto = numeroQuarto;
-        this.checkin = checkin;
-        this.checkout = checkout;
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
     }
 
-    public Integer getNumeroQuarto() {
-        return numeroQuarto;
+    public Integer getRoomNumber() {
+        return roomNumber;
     }
 
-    public void setNumeroQuarto(Integer numeroQuarto) {
-        this.numeroQuarto = numeroQuarto;
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
     }
 
-    public Date getCheckin() {
-        return checkin;
+    public Date getCheckIn() {
+        return checkIn;
     }
 
-    public Date getCheckout() {
-        return checkout;
+    public Date getCheckOut() {
+        return checkOut;
     }
 
-    public long duracao() {
-        long diff = checkout.getTime() - checkin.getTime();
-       return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    public long duration() {
+        long diff = checkOut.getTime() - checkIn.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
-    public void updateDates(Date checkin, Date checkout) {
-        this.checkin = checkin;
-        this.checkout = checkout;
+    public void updateDates(Date checkIn, Date checkOut) {
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
     }
 
     @Override
     public String toString() {
-        return "Quarto "
-                + numeroQuarto
-                + ", check in: "
-                + sdf.format(checkin)
+        return "Room "
+                + roomNumber
+                + ", check-in: "
+                + sdf.format(checkIn)
                 + ", check-out: "
-                + sdf.format(checkout)
+                + sdf.format(checkOut)
                 + ", "
-                + duracao()
+                + duration()
                 + " nights";
     }
 }
